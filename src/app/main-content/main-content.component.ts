fcd6fe3cecc25a028397feea6f170fc743c9a561
evanebert98 <evanebert@mail.fresnostate.edu>
2020-05-11 00:13:33 -0700

    Lab 4

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IStudent {
+  id:number;
+  firstName: string;
+  LastName: string;
+  course: string;
+}
+
 @Component({
   selector: 'main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+studentArray: Array<object> = [];
+
 
   constructor() {
     this.students[0] = {
@@ -28,7 +37,39 @@ export class MainContentComponent implements OnInit {
     }
    }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.studentArray = [
+      {
+        id: 1,
+        firstName: 'Evan',
+        lastName: 'Ebert',
+        course:'Programming'
+      },
+      {
+        id: 2,
+        firstName: 'Mike',
+        lastName: 'Tyson',
+        course:'Boxing'
+      },
+      {
+        id: 3,
+        firstName: 'Mohammid',
+        lastName: 'Ali',
+        course:'Super Boxing'
+      }
+    ];
+  } 
+  addStudent () {
+    this.studentArray.unshift({
+      id: 1,
+      firstName: 'Evan',
+      lastName: 'Ebert',
+      course:'Programming' 
+    });
+  }
+  removeStudent(index: number) {
+    console.log('index', index);
+    this.studentArray.splice(index, 2);
   }
 
 }
